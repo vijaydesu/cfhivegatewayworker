1c8f8464dbb0c2fc2e9da433e716cfaafa4f3716
vijay desu <vijay.b.desu@gmail.com>
2025-01-20 21:39:00 -0600

    Hive GraphQL Server using Hive yoga Framework

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
 Deploy HiveGateway using Cloudflare workers
+
+
+/* plugins(ctx) {
+					return [
+					  useOpenTelemetry({
+						logger: ctx.logger,
+						exporters: [
+						  createOtlpHttpExporter({
+							url: "https://otlp.nr-data.net:4318",
+							headers: {
+							  apikey: '4c6dec88985c833a69df36f79536ef62FFFFNRAL',
+							  OTEL_EXPORTER_OTLP_HEADERS:'apikey=4c6dec88985c833a69df36f79536ef62FFFFNRAL'
+							},
+						  }),
+						],
+						serviceName: "cf-hive-gateway-muletest", // Optional, the name of your service
+						//tracer: 'CFHiveGatewayCustomTracer', // Optional, a custom tracer to use
+						inheritContext: true, // Optional, whether to inherit the context from the incoming request
+						propagateContext: true, // Optional, whether to propagate the context to the outgoing requests
+						// Optional config to customize the spans. By default all spans are enabled.
+						spans: {
+						  http: true, // Whether to track the HTTP request/response
+						  graphqlParse: true, // Whether to track the GraphQL parse phase
+						  graphqlValidate: true, // Whether to track the GraphQL validate phase
+						  graphqlExecute: true, // Whether to track the GraphQL execute phase
+						  subgraphExecute: true, // Whether to track the subgraph execution phase
+						  upstreamFetch: true, // Whether to track the upstream HTTP requests
+						},
+					  }),
+					];
+				  }, */
+
+      /* OpenTelemetry: {
+					exporters: [
+						createOtlpHttpExporter({
+							url: 'https://otlp.nr-data.net:4318',
+							headers: {
+								'OTEL_EXPORTER_OTLP_HEADERS': 'apikey=4c6dec88985c833a69df36f79536ef62FFFFNRAL',
+								'apikey': '4c6dec88985c833a69df36f79536ef62FFFFNRAL',
+								'OTEL_SERVICE_NAME': 'cf-hive-gateway-muletest',
+								'OTEL_RESOURCE_ATTRIBUTES': 'service.instance.id=12345',
+								'OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT': '4095',
+								'OTEL_EXPORTER_OTLP_COMPRESSION': 'gzip',
+								'OTEL_EXPORTER_OTLP_PROTOCOL': 'http/protobuf'
+							}
+						}),
+						createStdoutExporter()
+					],
+					serviceName: 'cf-hive-gateway-muletest', // Optional, the name of your service
+					//tracer: 'CFHiveGatewayCustomTracer', // Optional, a custom tracer to use
+					inheritContext: true, // Optional, whether to inherit the context from the incoming request
+					propagateContext: true, // Optional, whether to propagate the context to the outgoing requests
+					// Optional config to customize the spans. By default all spans are enabled.
+					spans: {
+						http: true, // Whether to track the HTTP request/response
+						graphqlParse: true, // Whether to track the GraphQL parse phase
+						graphqlValidate: true, // Whether to track the GraphQL validate phase
+						graphqlExecute: true, // Whether to track the GraphQL execute phase
+						subgraphExecute: true, // Whether to track the subgraph execution phase
+						upstreamFetch: true // Whether to track the upstream HTTP requests
+					}
+				}, */
--- cfhivegatewayworker/src/index.ts
+++ cfhivegatewayworker/src/index.ts
@@ -20,16 +20,18 @@
 	},
 } satisfies ExportedHandler<Env>;*/
 
-import { createGatewayRuntime } from "@graphql-hive/gateway-runtime";
+import { createGatewayRuntime, DefaultLogger } from "@graphql-hive/gateway-runtime";
 import http from "@graphql-mesh/transport-http";
-import Logger from "./CustomLogger";
+//import Logger from "./CustomLogger";
 // import { useOpenTelemetry } from "@graphql-hive/gateway";
 import CloudflareKVCacheStorage from "@graphql-mesh/cache-cfw-kv";
+import {LogLevel} from "@graphql-mesh/utils";
+import {Logger} from "@graphql-mesh/types";
 
 export default {
   async fetch(request: any, env: any, ctx: any) {
     //console.log("Global KV Namespace : "+globalThis.HiveGateway);
-    const logger = new Logger();
+    //const logger = new Logger();
     const gateway = createGatewayRuntime({
       // All options available in `gateway.config.ts` configuration can also be passed here.
       supergraph: {
@@ -40,7 +42,9 @@ export default {
           "https://cdn.graphql-hive.com/artifacts/v1/e41e9e21-6705-4502-b0e4-1d04dbf6da43/supergraph",
         // The API key provided by Hive Registry
         key: "hv2ZGYyYmQxNGEtOTNlNS00ZjIwLTk3ZWYtZmU3OWVkMDdlNzFjOmM1Zjc5YjBjMjA4NGE4MzY5MDcyMTEzOWE4NjAxYjFiMzRkNjYyN2I=",
+		
       },
+	  pollingInterval: 3000,
       //supergraph,
       transports: {
         http, // http transport is required for subgraphs using standard GraphQL over HTTP.
@@ -65,77 +69,12 @@ export default {
       healthCheckEndpoint: "/healthcheck",
       readinessCheckEndpoint: "/readiness",
 
-      /* plugins(ctx) {
-					return [
-					  useOpenTelemetry({
-						logger: ctx.logger,
-						exporters: [
-						  createOtlpHttpExporter({
-							url: "https://otlp.nr-data.net:4318",
-							headers: {
-							  apikey: '4c6dec88985c833a69df36f79536ef62FFFFNRAL',
-							  OTEL_EXPORTER_OTLP_HEADERS:'apikey=4c6dec88985c833a69df36f79536ef62FFFFNRAL'
-							},
-						  }),
-						],
-						serviceName: "cf-hive-gateway-muletest", // Optional, the name of your service
-						//tracer: 'CFHiveGatewayCustomTracer', // Optional, a custom tracer to use
-						inheritContext: true, // Optional, whether to inherit the context from the incoming request
-						propagateContext: true, // Optional, whether to propagate the context to the outgoing requests
-						// Optional config to customize the spans. By default all spans are enabled.
-						spans: {
-						  http: true, // Whether to track the HTTP request/response
-						  graphqlParse: true, // Whether to track the GraphQL parse phase
-						  graphqlValidate: true, // Whether to track the GraphQL validate phase
-						  graphqlExecute: true, // Whether to track the GraphQL execute phase
-						  subgraphExecute: true, // Whether to track the subgraph execution phase
-						  upstreamFetch: true, // Whether to track the upstream HTTP requests
-						},
-					  }),
-					];
-				  }, */
-
-      /* OpenTelemetry: {
-					exporters: [
-						createOtlpHttpExporter({
-							url: 'https://otlp.nr-data.net:4318',
-							headers: {
-								'OTEL_EXPORTER_OTLP_HEADERS': 'apikey=4c6dec88985c833a69df36f79536ef62FFFFNRAL',
-								'apikey': '4c6dec88985c833a69df36f79536ef62FFFFNRAL',
-								'OTEL_SERVICE_NAME': 'cf-hive-gateway-muletest',
-								'OTEL_RESOURCE_ATTRIBUTES': 'service.instance.id=12345',
-								'OTEL_ATTRIBUTE_VALUE_LENGTH_LIMIT': '4095',
-								'OTEL_EXPORTER_OTLP_COMPRESSION': 'gzip',
-								'OTEL_EXPORTER_OTLP_PROTOCOL': 'http/protobuf'
-							}
-						}),
-						createStdoutExporter()
-					],
-					serviceName: 'cf-hive-gateway-muletest', // Optional, the name of your service
-					//tracer: 'CFHiveGatewayCustomTracer', // Optional, a custom tracer to use
-					inheritContext: true, // Optional, whether to inherit the context from the incoming request
-					propagateContext: true, // Optional, whether to propagate the context to the outgoing requests
-					// Optional config to customize the spans. By default all spans are enabled.
-					spans: {
-						http: true, // Whether to track the HTTP request/response
-						graphqlParse: true, // Whether to track the GraphQL parse phase
-						graphqlValidate: true, // Whether to track the GraphQL validate phase
-						graphqlExecute: true, // Whether to track the GraphQL execute phase
-						subgraphExecute: true, // Whether to track the subgraph execution phase
-						upstreamFetch: true // Whether to track the upstream HTTP requests
-					}
-				}, */
-
-      //logging: LogLevel.info
-      logging: logger,
+      logging: LogLevel.debug,
       cache: new CloudflareKVCacheStorage({
-        logger,
+       
         namespace: env.HiveGateway,
       }),
-      /*cache: new CFWorkerKVCache({
-					logger,
-					namespace: env.HiveGateway,
-				  }),*/
+      
       responseCaching: {
         // global cache
         session: () => null,
